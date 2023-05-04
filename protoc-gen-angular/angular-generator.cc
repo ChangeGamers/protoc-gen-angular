24fe9b8edc886cdb507fa2875d11d3159ab3d10a
Finn Tobin <finngoodfellow@gmail.com>
2023-05-04 09:11:45 -0700

    revert change

--- protoc-gen-angular/angular-generator.cc
+++ protoc-gen-angular/angular-generator.cc
@@ -575,7 +575,7 @@ namespace {
     for(auto& pair : fileToDescriptorMap) {
       auto filename = pair.first;
       auto descriptors = pair.second;
-      auto importPath = removePathExtname(filename) + "_grpcjs/" + removePathExtname(filename) + "_grpc_pb";
+      auto importPath = removePathExtname(filename) + "_grpcjs_pb/" + removePathExtname(filename) + "_grpc_pb";
 
       map<string, string> vars = {
         {"import_prefix", options.webImportPrefix},
